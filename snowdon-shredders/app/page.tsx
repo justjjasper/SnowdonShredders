7f7645234f95fe40683ad62ab3cdc7b7ec0b04b8
JASPER <jasjasper101@gmail.com>
2023-09-28 13:28:11 -0700

    Implement Python Cors to communicate with front end

--- snowdon-shredders/app/page.tsx
+++ snowdon-shredders/app/page.tsx
@@ -1,7 +1,30 @@
 import Image from 'next/image'
+'use client'
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
 export default function Home() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios('http://127.0.0.1:8000/snowboard');
+
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <main>Hello World</main>
+    <main>
+      <h1>Data from Server:</h1>
+      {JSON.stringify(data)}
+    </main>
   )
 }
--- snowdon-shredders/backend/snowdon_shredders_server/snowdon_shredders_server/settings.py
+++ snowdon-shredders/backend/snowdon_shredders_server/snowdon_shredders_server/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +123,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Allows all origin for DEVELOPMENT, change it during production.
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Replace with your frontend URL
+    # Add other allowed origins as needed
+]
+
+
+
+
